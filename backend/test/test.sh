bc27d053b7dc3fe1291d7f41eeeb5d81ef818bd5
Anna K <a.kuchebo@mail.ru>
2024-01-11 00:34:53 +0300

    add new test

--- backend/test/test.sh
+++ backend/test/test.sh
@@ -4,20 +4,26 @@
 curl -i -X POST -H 'Content-Type: application/json' -d '{"first_name": "Anna", "second_name": "K", "last_name": "V", "email": "test3@mail.ru", "phone": "87505197423", "password": "12334"}' http://localhost:8080/signup
 echo ""
 #test login - OK
-curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "Anna", "email": "test3@mail.ru", "password": "12334"}' http://localhost:8080/login
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "test3@mail.ru", "password": "12334"}' http://localhost:8080/login
 echo ""
 #test login - ERROR
-curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "Anna", "email": "b@mail.ru", "password": "1234"}' http://localhost:8080/login
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "b@mail.ru", "password": "1234"}' http://localhost:8080/login
 echo ""
 #test login - ERROR
-curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "Anna", "email": "test3@mail.ru", "password": "wrong-password"}' http://localhost:8080/login
-echo ""
-#test login - OK
-curl -i -X POST -H 'Content-Type: application/json' -d '{"name": "NoMatter", "email": "test3@mail.ru", "password": "12334"}' http://localhost:8080/login
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "test3@mail.ru", "password": "wrong-password"}' http://localhost:8080/login
 echo ""
 #test reset email - ERROR
-curl -i -X POST -H 'Content-Type: application/json' -d '{"first_name": "Anna", "second_name": "K", "last_name": "V", "email": "a3@mail.ru", "phone": "87505197429", "password": "test1234"}' http://localhost:8080/reset-email
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "a3@mail.ru", "phone": "87505197429", "password": "test1234"}' http://localhost:8080/reset-email
 echo ""
 #test reset email - OK
-curl -i -X POST -H 'Content-Type: application/json' -d '{"first_name": "Anna", "second_name": "K", "last_name": "V", "email": "new-test@mail.ru", "phone": "87505197423", "password": "12334"}' http://localhost:8080/reset-email
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "new-test@mail.ru", "phone": "87505197423", "password": "12334"}' http://localhost:8080/reset-email
+echo ""
+#test reset password - ERROR
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "wrong@mail.ru", "password": "12334"}' http://localhost:8080/reset-password
+echo ""
+#test reset password - OK - test without jwt
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "new-test@mail.ru", "password": "wrong"}' http://localhost:8080/reset-password
+echo ""
+#test reset password - OK - test without jwt
+curl -i -X POST -H 'Content-Type: application/json' -d '{"email": "new-test@mail.ru", "password": "12334"}' http://localhost:8080/reset-password
 echo ""
